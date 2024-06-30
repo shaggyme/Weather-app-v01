ac1e43c007732741c8ca82788a0c0dcb202dc6ef
shaggyme <alishaan089@gmail.com>
2024-06-30 20:45:43 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "fb2bf0c9beae651883996f1fa8e2880b";
+const API_KEY = "ENTER_YOUR_API_KEY";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
